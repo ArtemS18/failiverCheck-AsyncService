2f6495eb484e8a09877d0093babdcd43fbcb24a5
Artemii <rerttdtdf@gmail.com>
2025-12-16 16:37:01 +0300

    add branch

--- app/service.py
+++ app/service.py
@@ -1,6 +1,6 @@
 import asyncio
 from httpx import AsyncClient, Response
-from app.schema import ComponentsToSystemCalcList, ResultRequest
+from app.schema import Component, ComponentsToSystemCalcList, ResultRequest
 from app import config
 from logging import getLogger
 
@@ -9,14 +9,14 @@ log = getLogger(__name__)
 
 async def calculate_avalilable(items: ComponentsToSystemCalcList) -> float:
     system_available: float = 1
-    for i in items.components:
-        component = i.component_data
+    for el in items.components:
+        component: Component = el.component_data
         available = component.available
         if available == 0:
             if component.mtbf == 0 or component.mtbf + component.mttr == 0:
                 raise 
             available = float(component.mtbf) / float(component.mtbf + component.mttr)
-        component_available = float (1 - (float(1-available) ** float(i.replication_count)))
+        component_available = float (1 - (float(1-available) ** float(el.replication_count)))
         system_available *= component_available
     return system_available
 
